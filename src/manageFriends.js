0e546d50b1723d715bb1675383a4d48d57623cbf
mustafaguldag <guldag.mustafaa@gmail.com>
2021-07-27 19:46:56 +0300

    commit tree d0f4d456ffab22f18ba8089ccf8fdf37250c848a

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,7 @@
-export function manageFriends(state, action) {
+export function manageFriends(state= {friends: [] }, action) {
   // your code here
-}
+  switch(action.type){
+    case "friends/add": return { friends: [...state.friends, action.payload] }
+    case "friends/remove": return { friends: state.friends.filter(friend => friend.id !== action.payload) }; 
+    default: return state;
+}}
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,13 @@
-export function managePresents(state, action) {
+export function managePresents(state = {numberOfPresents: 0}, action) {
   // your code here
+  console.log(state)
+  let presents = state.numberOfPresents;
+
+  switch(action.type) {
+    case "presents/increase": 
+      presents = presents + 1;
+      return { numberOfPresents: presents }
+    default:
+      return state;
+  }
 }
